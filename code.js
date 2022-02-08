efce81d93462a57a7e84ffb8bc2dc2a6b41f23c9
Dominik Prinzensteiner <dominik.prinzensteiner@clickandlearn.at>
2022-02-08 14:48:52 +0100

    update

---
+++ code.js
@@ -0,0 +1,24 @@
+function generate() {
+    let lines = [];
+    let down = 0;
+    while (lines.length < 10000) {
+        let direction = getRandomValueOfArray(["down", "up", "forward"]);
+        let amount = getRandomIntegerInRange(1, 9);
+        if (direction == "up" && down - amount < 0) {
+            continue;
+        }
+        lines.push(direction + " " + amount);
+    }
+    return lines;
+}
+
+function getRandomValueOfArray(list) {
+    return list[Math.floor(Math.random() * list.length)];
+}
+
+function getRandomIntegerInRange(min, max) {
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min)) + min);
+}
+
+let lines = generate();
+console.log(lines, lines.join('\n'));
