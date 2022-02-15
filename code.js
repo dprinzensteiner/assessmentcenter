848b62bc71988a6daaeabb1ee5570172392a423e
Dominik Prinzensteiner <dominik.prinzensteiner@clickandlearn.at>
2022-02-15 10:50:12 +0100

    update

--- code.js
+++ code.js
@@ -1,3 +1,100 @@
+function parseInstructions(input) {
+    const lines = input.trim().split('\n')
+    const instructions = lines.map(line => {
+        const [direction, distance] = line.split(' ')
+        return {
+            direction,
+            distance: Number.parseInt(distance)
+        }
+    }).filter(n => n)
+    return instructions
+}
+
+const part1Commands = {
+    forward,
+    down,
+    up
+}
+
+const part2Commands = {
+    forward,
+    down: aimDown,
+    up: aimUp
+}
+
+function forward({distance}, position) {
+    position.x = position.x + distance
+    position.y = position.y + position.aim * distance
+}
+
+function down({distance}, position) {
+    position.y = position.y + distance
+}
+
+function up({distance}, position) {
+    position.y = position.y - distance
+}
+
+function aimDown({distance}, position) {
+    position.aim = position.aim + distance
+}
+
+function aimUp({distance}, position) {
+    position.aim = position.aim - distance
+}
+
+async function run(name, num, givensolution) {
+    fetch('assignments/' + name + '.txt').then((result) => {
+        return result.text();
+    }).then(async (content) => {
+        let sol = 0;
+        if(num == 1) {
+            sol = await solveForFirstStar(content)
+        } else {
+            sol = await solveForSecondStar(content)
+        }
+        if(givensolution == sol) {
+            alert("Lösung ist korrekt!");
+        } else {
+            alert("Lösung ist inkorrekt");
+        }
+    });
+
+
+}
+
+async function solveForFirstStar(input) {
+    const instructions = parseInstructions(input)
+    const position = {
+        x: 0,
+        y: 0,
+        aim: 0
+    }
+    while (instructions.length > 0) {
+        const next = instructions.shift()
+        const command = part1Commands[next.direction]
+        command(next, position)
+    }
+
+    return position.x * position.y
+}
+
+async function solveForSecondStar(input) {
+    const instructions = parseInstructions(input)
+    const position = {
+        x: 0,
+        y: 0,
+        aim: 0
+    }
+    while (instructions.length > 0) {
+        const next = instructions.shift()
+        const command = part2Commands[next.direction]
+        command(next, position)
+    }
+
+    return position.x * position.y;
+}
+
 function generate() {
     let lines = [];
     let down = 0;
@@ -53,8 +150,13 @@ participants.forEach((surname) => {
         </p>
 
         <div class="input-group mb-3">
-            <input type="text" class="form-control" id='abgabe-${surname}' placeholder="abgabe-${surname}" aria-label="abgabe" aria-describedby="basic-addon-${surname}">
-            <button class="btn btn-outline-secondary button-addon" type="button" id="button-addon-${surname}">überprüfen</button>
+            <input type="number" class="form-control" id='abgabe-${surname}' placeholder="aufgabe-teil-1-${surname}" aria-label="abgabe" aria-describedby="basic-addon-${surname}">
+            <button class="btn btn-outline-secondary button-addon-1" type="button" id="button-addon-${surname}">überprüfen</button>
+        </div>
+
+        <div class="input-group mb-3">
+            <input type="number" class="form-control" id='abgabe-${surname}' placeholder="aufgabe-teil-2-${surname}" aria-label="abgabe" aria-describedby="basic-addon-${surname}">
+            <button class="btn btn-outline-secondary button-addon-2" type="button" id="button-addon-${surname}">überprüfen</button>
         </div>
       </div>
     </div>
@@ -67,9 +169,15 @@ participants.forEach((surname) => {
 
 });
 
-document.querySelectorAll('.button-addon').forEach((element) => {
+document.querySelectorAll('.button-addon-1').forEach((element) => {
     element.addEventListener('click', (event) => {
         let input = element.previousElementSibling.value;
-        console.log(event, input)
+        run(element.id.split('-')[2], 1, input)
+    })
+})
+document.querySelectorAll('.button-addon-2').forEach((element) => {
+    element.addEventListener('click', (event) => {
+        let input = element.previousElementSibling.value;
+        run(element.id.split('-')[2], 2, input)
     })
 })
