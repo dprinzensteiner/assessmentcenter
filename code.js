d077a3fede5954614568b3eb472e95c202534297
Dominik Prinzensteiner <dominik.prinzensteiner@clickandlearn.at>
2022-02-15 10:34:58 +0100

    update

--- code.js
+++ code.js
@@ -32,20 +32,6 @@ let participants = [
     "hartl"
 ];
 
-/* participants.forEach((surname) => {
-    let lines = generate();
-    fs.writeFile('assignments/'+surname+'.txt', lines.join('\n'), { flag: 'w+' }, err => {
-        if (err) {
-          console.error(err)
-          return
-        }
-        //file written successfully
-      })
-    
-}) */
-
-//console.log(lines, lines.join('\n'));
-
 document.querySelector('#angaben')
 
 participants.forEach((surname) => {
@@ -56,17 +42,19 @@ participants.forEach((surname) => {
     let el = `
     <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-${surname}" aria-expanded="false" aria-controls="flush-collapse-${surname}">
         assignments/${surname}.txt
       </button>
     </h2>
-    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+    <div id="flush-collapse-${surname}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-        <a href="assignments/${surname}.txt" target='_blank'>assignments/${surname}.txt</a>
+        <p>
+            <a href="assignments/${surname}.txt" target='_blank' class="d-flex justify-content-between align-items-start">assignments/${surname}.txt <i class="bi bi-box-arrow-up-right"></i></a>
+        </p>
 
         <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon-${surname}">@</span>
-            <input type="text" class="form-control" id='abgabe-${surname}' placeholder="abgabe" aria-label="abgabe" aria-describedby="basic-addon-${surname}">
+            <input type="text" class="form-control" id='abgabe-${surname}' placeholder="abgabe-${surname}" aria-label="abgabe" aria-describedby="basic-addon-${surname}">
+            <button class="btn btn-outline-secondary button-addon" type="button" id="button-addon-${surname}">überprüfen</button>
         </div>
       </div>
     </div>
@@ -78,3 +66,10 @@ participants.forEach((surname) => {
     parent.appendChild(ele);
 
 });
+
+document.querySelectorAll('.button-addon').forEach((element) => {
+    element.addEventListener('click', (event) => {
+        let input = element.previousElementSibling.value;
+        console.log(event, input)
+    })
+})
